0b52fc07982ce33a7cdbfd0639d3d0b642ba3af0
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-08-09 07:13:27 +0300

    Add MyRoot(BoxLayout)

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
+import random
 
 kivy.require('1.9.0')
+
+
+class MyRoot(BoxLayout):
+
+    def __init__(self):
+        super(MyRoot, self).__init__()
+
+    def generate_number(self):
+        self.random_label.text = str(random.randint(0, 32))
 
 
 class DarishkaAMSRandom(App):
 
     def build(self):
         # return Label(text="hello")
-        return BoxLayout()
+        return MyRoot()
 
 
 if __name__ == "__main__":
